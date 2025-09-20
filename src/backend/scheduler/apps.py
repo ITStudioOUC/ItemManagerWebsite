1468f412643a07ef00ac0d1a274333a0dac54b6a
Yaosanqi137 <zxc18023571263@outlook.com>
2025-09-20 21:27:32 +0800

    fix: fix some backend init bug while first start

--- src/backend/scheduler/apps.py
+++ src/backend/scheduler/apps.py
@@ -6,5 +6,8 @@ class SchedulerConfig(AppConfig):
     name = 'scheduler'
 
     def ready(self):
-        from . import jobs
-        jobs.start_scheduler()
+        # 只有在runserver命令时才启动调度器，避免在migrate等命令时启动 :(
+        import sys
+        if 'runserver' in sys.argv:
+            from . import jobs
+            jobs.start_scheduler()
